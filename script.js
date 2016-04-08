3cb6eebb712f1f025316d8583b3015f58b5aa977
Zenon <lucaszenonmarantes@hotmail.com>
2016-04-08 12:17:46 -0300

    Added files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+$(document).ready(function(e){
+	$(".menuPrincipal a").click(function(e){
+		e.preventDefault();
+		var href = $(this).attr(’href’);
+		$(".conteudo").load(href + ".conteudo");
+	});
+}) 
+
+function dialogo(){
+		$("#dialog-confirm").dialog({
+			resizable:false,
+			modal:true,
+			width:400,
+			height:200,
+		buttons: { 
+	"Confirma" : function(){
+	$(this ).dialog( "close");
+	},
+	Cancelar: function(){
+		$( this ).dialog( "close" );
+			}
+			}
+		});
+	});
+
+function verifica() {
+var teste = 0;
+var erro = 0;
+var nomeTexto = document.querySelector("#nome").value ;
+var vetnom
+var truzao
+var email = document.querySelector("#email").value ;
+if(nomeTexto.length<3){
+erro = 1;
+}
+var x
+for(x=0;x<nomeTexto.length;x++{
+if(nomeTexto.substring[x] == " "){
+truzao = true; 
+
+}
+else{
+erro=1
+}
+if(truzao == true){
+vetnom = nomeTexto.split(" ")
+
+}
+else{
+
+
+}
+}
+}
