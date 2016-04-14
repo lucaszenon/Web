2672bdbf76d6cbd1305ef86e5e8729e69a5906a5
Zenon <lucaszenonmarantes@hotmail.com>
2016-04-14 12:20:41 -0300

    Added files via upload

--- SiteIron/script.js
+++ SiteIron/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(e){
-	$("#dialog-confirm").hide();
+    $("#dialog-confirm").hide();
 	$(".menuPrincipal a").click(function(e){
 		e.preventDefault();
 		var href = $(this).attr('href');
@@ -14,13 +14,13 @@ function dialogo(){
 			width:400,
 			height:200,
 		buttons: { 
-	"Confirma":function(){
-		verifica();
-	$(this).dialog("close");
-
-	},
+	"Confirma" : function() {
+    verifica();        
+	$(this ).dialog( "close");
+	verifica();
+    },
 	Cancelar: function(){
-		$(this).dialog("close");
+		$( this ).dialog( "close" );
 			}
 			}
 		});
@@ -29,27 +29,58 @@ function dialogo(){
 function verifica() {
 var teste = 0;
 var erro = 0;
-var nomeTexto = document.querySelector("#nome").value ;
+var nome = document.querySelector("#nome").value;
 var vetnom;
 var truzao;
-var email = document.querySelector("#email").value ;
-if(nomeTexto.length<3){
-erro = 1;
-}
-var x
-for(x=0;x<nomeTexto.length;x++){
-if(nomeTexto.substring[x] == " "){
+var email = document.querySelector("#email").value;
+var vetemail;
+    
+var x;
+for(x=0;x<nome.length;x++){
+if(nome.substring[x] == " "){
 truzao = true; 
 
 }
-else{
-erro=1;
-}
-if(truzao == true){
-vetnom = nomeTexto.split(" ")
 
 }
-else{
+if(truzao == true){
+vetnom = nome.split(" ")
+if(vetnom[0].length<3){
+erro = 1;
 }
+    if(vetnom[1].length<4){
+erro = 1;    
+}
+}
+else{
+erro=1;
+
+}
+truzao =false;
+for(x=0;x<nome.length;x++){
+if(nome.substring[x] == "@"){
+truzao = true; 
+
+}
+
+
+}
+if(truzao == true){
+vetemail = nome.split("@")
+if(vetemail[0].length<3){
+erro = 1;
+}
+
+if(vetemail[1].length<4){
+erro = 1;    
+}
+}
+else{
+erro=1
+
+}
+if(erro==1){
+    
+    alert("Ha um erro em seu codigo")
 }
 }
